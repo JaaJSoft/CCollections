960b890ebd620fd16882539a7ee360ee793f5ac4
pchopinet <pierre.chopinet97@gmail.com>
2018-05-04 10:53:57 +0200

    add test for PriorityQueue & update test for ArrayList

--- test/testArray.c
+++ test/testArray.c
@@ -3,23 +3,26 @@
 //
 
 #include <ArrayList.h>
+#include <time.h>
 
 int main() {
 
     ArrayList a = newArrayList(sizeof(int));
-    int b = 0, c = 5, d = 42;
+    srand(time(NULL));
+    printf("Expected : \n");
+    for (int i = 0; i < 80; i++) {
+        int *t = malloc(sizeof(int));
+        *t = rand() % 100;
+        printf("%d ", *t);
+        ArrayListAppend(a, t);
+    }
+    printf("\nFind : \n");
+    for (int i = 0; i < 80; i++) {
+        int *b = ArrayListGet(a, i);
+        printf("%d ", *b);
+        free(b);
+    }
 
-    ArrayListAppend(a, &b);
-    ArrayListAppend(a, &c);
-    ArrayListAppend(a, &c);
-    ArrayListAppend(a, &b);
-    ArrayListSet(a, 2, &d);
-    ArrayListDisplay(a);
-    ArrayListRemoveValue(a, 2);
-    ArrayListDisplay(a);
-
-    ArrayListAppend(a, &d);
-    ArrayListDisplay(a);
     ArrayListDelete(a);
     return 0;
 }
--- test/testPriorityQueue.c
+++ test/testPriorityQueue.c
@@ -2,6 +2,22 @@
 // Created by pierre on 01/05/18.
 //
 
+#include <PriorityQueue.h>
+#include <time.h>
+
 int main() {
+    PriorityQueue p = newPriorityQueue();
+    srand(time(NULL));
+    for (int i = 0; i < 80; i++) {
+        int *t = malloc(sizeof(int));
+        *t = rand()%100;
+        PriorityQueueAdd(p, t, 1);
+    }
+    for (int i = 0; i < 80; i++) {
+        int *a = PriorityQueuePop(p);
+        printf("%d ", *a);
+        free(a);
+    }
+
 
 }
